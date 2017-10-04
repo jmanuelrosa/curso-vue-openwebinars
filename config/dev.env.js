79f9bc1f3acaf64cb1bb0e32c7aa655688891108
José Manuel Rosa Moncayo <josemanuel.rosamoncayo@gmail.com>
2017-10-04 10:26:22 +0200

    Remove token

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
-  TOKEN: '"25927eca0050c5292ddcce6acf21ed309d1b4a90"'
+  TOKEN: '"ADD-YOUR-TOKEN"'
 })
