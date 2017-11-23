3b6ee52e0b6ac8da07185e643b3da90d0e59b43b
rafagarcia <rafagarcia@gmail.com>
2017-11-23 20:04:15 +0100

    Update main.js
    Fixed error in categories array

--- my-tasks/scripts/main.js
+++ my-tasks/scripts/main.js
@@ -11,6 +11,7 @@ new Vue({
           { name: 'Vuex', striked: false },
           { name: 'Vue-router', striked: false },
           { name: 'Vue-axios', striked: true }
+        ]
       },
       {
         title: 'Recipes',
